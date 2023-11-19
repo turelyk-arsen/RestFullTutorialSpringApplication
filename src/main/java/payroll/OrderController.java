b9b45a72f3836a735904bd4ca733b68ebea4cbaf
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-11-19 21:47:29 +0100

    add all RESTfull from Spring tutorial

--- src/main/java/payroll/OrderController.java
+++ src/main/java/payroll/OrderController.java
@@ -5,16 +5,21 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.*;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-
+@RestController
 public class OrderController {
 	
 	private final OrderRepository orderRepository;
@@ -41,12 +46,47 @@ public class OrderController {
 		return assembler.toModel(order);
 	}
 	
-	@PostMapping("/orders/{id}")
+	@PostMapping("/orders")
 	ResponseEntity<EntityModel<Order>> newOrder (@RequestBody Order order) {
 		order.setStatus(Status.IN_PROGRESS);
 		Order newOrder = orderRepository.save(order);
 		return ResponseEntity
 				.created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
 				.body(assembler.toModel(newOrder));
+	}
+	
+	@PutMapping("/orders/{id}/complete")
+	ResponseEntity<?> complete(@PathVariable Long id) {
+		Order order = orderRepository.findById(id)
+				.orElseThrow(()-> new OrderNotFoundException(id));
+		
+		if(order.getStatus() == Status.IN_PROGRESS) {
+			order.setStatus(Status.COMPLETED);
+			return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+		}
+		
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+				.body(Problem.create()
+						.withTitle("Method not allowed")
+						.withDetail("You can't complete an order that is in the " + order.getStatus() + "status"));
+	}
+	
+	@DeleteMapping("/orders/{id}/cancel")
+	ResponseEntity<?> cancel(@PathVariable Long id) {
+		Order order = orderRepository.findById(id)
+				.orElseThrow(()-> new OrderNotFoundException(id));
+		
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			order.setStatus(Status.CANCELLED);
+			return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+		}
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+				.body(Problem.create()
+				.withTitle("Method not allowed")
+				.withDetail("You can't cancel an order that is in the " + order.getStatus() + "status"));
 	}
 }
